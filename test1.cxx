25abff4fc2897ce4b35423db58cda2070346cfc8
roei-bloch <roei.bloch@campus.technion.ac.il>
2023-05-24 13:25:13 +0300

    add sum_tests
    Co-authored-by: nirhersh <nirhersh@users.noreply.github.com>

--- test1.cxx
+++ test1.cxx
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cassert>
 
+void sum_tests();
+
 using namespace std;
 
 int main() {
@@ -15,7 +17,45 @@ int main() {
     x = get_weight();
 	cout << "new weight: " << x << endl;
 	assert(x == 5);
-    cout << "===== SUCCESS =====" << endl;
+    cout << "set & get test: PASS" << endl;
+
+	sum_tests();
+	cout << "ancestor sum test: PASS" << endl;
     return 0;
 }
 
+void sum_tests()
+{
+	set_weight(0);
+	int anc_sum = get_ancestor_sum();
+	cout <<"ancestor sum: " << anc_sum << endl;
+	assert(anc_sum == 0);
+	
+	set_weight(3);
+	anc_sum = get_ancestor_sum();
+	cout <<"ancestor sum: " << anc_sum << endl;
+	assert(anc_sum == 3);
+
+	pid_t pid = fork();
+	if(pid == 0) // first row child
+	{
+		assert(get_ancestor_sum() == 6);
+		set_weight(5);
+		assert(get_ancestor_sum() == 8);
+		pid_t pid1 = fork();
+		if(pid1 == 0){ // second row child
+			assert(get_ancestor_sum() == 13);
+			set_weight(0);
+			assert(get_ancestor_sum() == 8);
+		}
+	} else { // daddy
+		assert(get_ancestor_sum() == 3);
+		pid_t pid2 = fork();
+		if(pid2 == 0){ // second child first row
+			assert(get_ancestor_sum() == 6);
+			set_weight(10);
+			assert(get_ancestor_sum() == 13);
+		}
+	}
+
+}
