4cd906d1a50b800fd2c3363367db7d3194e11b6f
roei-bloch <roei.bloch@campus.technion.ac.il>
2023-05-24 14:13:49 +0300

    lol

--- test1.cxx
+++ test1.cxx
@@ -73,6 +73,7 @@ void heaviest_descendant_test()
 {
 	set_weight(0);
 	int dec_hav = get_heaviest_descendant();
+	cout << "parent pid: " << getpid() << endl;
 	cout << "the very parent heaviest " << dec_hav << endl;
 	assert(dec_hav == -ECHILD);	
 	pid_t pid1 = fork();
